25e6e6a8a0970ce1244097373d186ca08d898424
Jeff Hussmann <jeff.hussmann@gmail.com>
2018-04-24 17:32:49 -0700

    changes svg standalone default to False

--- svg.py
+++ svg.py
@@ -31,6 +31,7 @@ before_svg = '''\
     max-width: 100%;
 }
 </style>
+
 </head>
 <body>
 <div style="height:5000px;">'''
@@ -40,17 +41,21 @@ after_svg = '''\
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 <script>
     $(document).ready(function(){
         $('[data-toggle="popover"]').popover();
+
         $('[data-toggle="popover"]').on('show.bs.popover', function() {
           $("path", this).attr('stroke-opacity', '0.5');
         });
+
         $('[data-toggle="popover"]').on('hide.bs.popover', function() {
           $("path", this).attr('stroke-opacity', '0.05');
         });
     });
 </script>
+
 </body>'''
 
 def length_plot_with_popovers(exp, standalone=False, container_selector='body'):
