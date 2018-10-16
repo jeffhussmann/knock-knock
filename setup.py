d2ee0ad2778bc12dbf0cb018cbef4cb72d6338f6
Jeff Hussmann <jeff.hussmann@gmail.com>
2018-10-16 08:40:18 -0700

    adds build to .gitignore

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-/home/jah/projects/britt/setup.py
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+ext_modules = [
+    Extension('collapse_cython', ['collapse_cython.pyx']),
+]
+
+setup(
+  name = 'cython stuff',
+  cmdclass = {'build_ext': build_ext},
+  ext_modules = ext_modules
+)
