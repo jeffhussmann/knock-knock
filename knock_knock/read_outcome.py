9c504e3b7c30cbceb71a7be977cc648e5cf02e2f
jeffhussmann <jeff.hussmann@gmail.com>
2019-12-29 15:17:24 -0800

    gives Outcome access to columns static variable

--- knock_knock/read_outcome.py
+++ knock_knock/read_outcome.py
@@ -1,12 +1,14 @@
-def Outcome_factory(columns, converters):
+def Outcome_factory(columns_arg, converters_arg):
     field_index_to_converter = {}
-    for i, c in enumerate(columns):
-        if c in converters:
-            field_index_to_converter[i] = converters[c]
+    for i, c in enumerate(columns_arg):
+        if c in converters_arg:
+            field_index_to_converter[i] = converters_arg[c]
     
     class Outcome():
+        columns = columns_arg
+
         def __init__(self, *args):
-            for name, arg in zip(columns, args):
+            for name, arg in zip(columns_arg, args):
                 setattr(self, name, arg)
 
         @classmethod
@@ -22,16 +24,21 @@ def Outcome_factory(columns, converters):
             return (self.category, self.subcategory, self.details)        
 
         def __str__(self):
-            row = [str(getattr(self, k)) for k in columns]
+            row = [str(getattr(self, k)) for k in columns_arg]
             return '\t'.join(row)
+
+        def __repr__(self):
+            return str(self)
     
     return Outcome
 
-Outcome = Outcome_factory(['query_name',
-                           'length',
-                           'category',
-                           'subcategory',
-                           'details',
-                           ],
-                          {'length': int},
-                         )
+Outcome = Outcome_factory(
+    columns_arg=[
+        'query_name',
+        'length',
+        'category',
+        'subcategory',
+        'details',
+    ],
+    converters_arg={'length': int},
+)
