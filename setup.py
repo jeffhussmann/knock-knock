165420e37680242f2c3c80015f66c78065d0fdd3
Jeff Hussmann <jeff.hussmann@gmail.com>
2021-06-23 10:38:25 -0400

    increments version to 0.3.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ for fn in targets_dir.glob('*.csv'):
 
 setup(
     name='knock_knock',
-    version='0.3',
+    version='0.3.1',
     url='https://pypi.org/projects/knock-knock',
 
     author='Jeff Hussmann',
