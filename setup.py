eb8fc224c7ac4a4d0ffc3fbf1ec17fb72d133bb9
Jeff Hussmann <jeff.hussmann@gmail.com>
2022-03-08 14:26:48 -0500

    bumps version to 0.3.8

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ for fn in targets_dir.glob('*.csv'):
 
 setup(
     name='knock_knock',
-    version='0.3.7',
+    version='0.3.8',
     url='https://pypi.org/projects/knock-knock',
 
     author='Jeff Hussmann',
