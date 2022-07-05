cb989b5cc7f65712d728ceaae603089332b94120
Jeff Hussmann <jeff.hussmann@gmail.com>
2022-07-05 07:24:40 -0400

    fixes misunderstanding in multiprocess logging

--- knock_knock/parallel.py
+++ knock_knock/parallel.py
@@ -1,45 +1,48 @@
 import logging
 import logging.handlers
 import multiprocessing
-import threading
 
 def setup_logging_then_call(queue, func, args):
-    ''' Register a QueueHandler with queue, then call func
+    ''' Register a QueueHandler connected queue, then call func
     with args. Intended as a pickleable function to be given
     to Pool.starmap.
     '''
     queue_handler = logging.handlers.QueueHandler(queue)
+    queue_handler.setLevel(logging.DEBUG)
+
     logger = logging.getLogger()
+
+    logger.setLevel(logging.DEBUG)
+
+    existing_handlers = list(logger.handlers)
+
+    for handler in existing_handlers:
+        logger.removeHandler(handler)
+
     logger.addHandler(queue_handler)
+
     func(*args)
-    
+
+    logger.removeHandler(queue_handler)
+
+    for handler in existing_handlers:
+        logger.addHandler(handler)
+
 class PoolWithLoggerThread:
     ''' A context manager for a combination of a multiprocessing.Pool
     and a threaded handler for logging from the Pool's processes.
     '''
     
-    def __init__(self, processes):
+    def __init__(self, processes, logger):
         # I don't really understand multiprocessing.Queue() vs.
         # multiprocessing.Manager().Queue(), but only the latter works here.
         manager = multiprocessing.Manager()
         self.queue = manager.Queue()
 
-        # Set up a thread to receive messages from the queue and log them.
-        self.thread = threading.Thread(target=self.process_queue)
+        self.queue_listener = logging.handlers.QueueListener(self.queue, *logger.handlers)
 
         self.pool = multiprocessing.Pool(processes=processes, maxtasksperchild=1)
         
-    def process_queue(self):
-        logger = logging.getLogger()
-        
-        while True:
-            record = self.queue.get()
-            
-            if record is None:
-                break
-                
-            logger.handle(record)
-            
     def starmap(self, func, iterable):
         ''' Provides the same interface as Pool.starmap, but connects each
         of the Pool's processes to the logging queue before executing func.
@@ -48,12 +51,10 @@ class PoolWithLoggerThread:
         self.pool.starmap(setup_logging_then_call, arg_tuples, 1)
     
     def __enter__(self):
+        self.queue_listener.start()
         self.pool.__enter__()
-        self.thread.start()
         return self
         
     def __exit__(self, exception_type, exception_value, exception_traceback):
-        self.queue.put(None)
-        self.thread.join()
-
         self.pool.__exit__(exception_type, exception_value, exception_traceback)
+        self.queue_listener.stop()
