08b6f5fce9afad4dc930af0cacb032ab9efdfdf7
Jeff Hussmann <jeff.hussmann@gmail.com>
2022-07-22 13:08:02 -0400

    increases compatibility of PoolWithLoggerThread with Pool's API

--- knock_knock/parallel.py
+++ knock_knock/parallel.py
@@ -5,7 +5,7 @@ import multiprocessing
 def setup_logging_then_call(queue, func, args):
     ''' Register a QueueHandler connected queue, then call func
     with args. Intended as a pickleable function to be given
-    to Pool.starmap.
+    to Pool.starmap or Pool.apply_async.
     '''
     queue_handler = logging.handlers.QueueHandler(queue)
     queue_handler.setLevel(logging.DEBUG)
@@ -47,6 +47,12 @@ class PoolWithLoggerThread:
 
         self.pool = multiprocessing.Pool(processes=processes, maxtasksperchild=1)
         
+    def apply_async(self, func, args):
+        ''' Provides the same interface as Pool.starmap, but connects each
+        of the Pool's processes to the logging queue before executing func.
+        '''
+        return self.pool.apply_async(setup_logging_then_call, (self.queue, func, args))
+
     def starmap(self, func, iterable):
         ''' Provides the same interface as Pool.starmap, but connects each
         of the Pool's processes to the logging queue before executing func.
@@ -62,3 +68,9 @@ class PoolWithLoggerThread:
     def __exit__(self, exception_type, exception_value, exception_traceback):
         self.pool.__exit__(exception_type, exception_value, exception_traceback)
         self.queue_listener.stop()
+
+    def close(self):
+        self.pool.close()
+
+    def join(self):
+        self.pool.join()
