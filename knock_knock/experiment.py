73030d87411eac6a8d9dd7f54d4ffec325ca50bf
Jeff Hussmann <jeff.hussmann@gmail.com>
2023-02-17 16:14:30 -0500

    makes color assignment not fail if number of experiments is large

--- knock_knock/experiment.py
+++ knock_knock/experiment.py
@@ -28,29 +28,32 @@ from hits.utilities import memoized_property
 from . import target_info, blast, visualize, outcome_record, svg, table, explore
 from . import layout as layout_module
 
-def color_groups():
-    starts_20c = np.arange(4) * 4
-    starts_20b = np.array([3, 1, 0, 2, 4]) * 4
+class ColorGroupCycler:
+    def __init__(self):
+        starts_20c = np.arange(4) * 4
+        starts_20b = np.array([3, 1, 0, 2, 4]) * 4
 
-    groups_20c = [bokeh.palettes.Category20c_20[start:start + 3] for start in starts_20c]
-    groups_20b = [bokeh.palettes.Category20b_20[start:start + 3] for start in starts_20b]
+        groups_20c = [bokeh.palettes.Category20c_20[start:start + 3] for start in starts_20c]
+        groups_20b = [bokeh.palettes.Category20b_20[start:start + 3] for start in starts_20b]
 
-    all_groups = (groups_20c + groups_20b) * 10
-
-    return all_groups
+        self.all_groups = (groups_20c + groups_20b)
+        
+    def __getitem__(self, key):
+        group_num, replicate = key
+        group = self.all_groups[group_num % len(self.all_groups)]
+        color = group[replicate % len(group)]
+        return color
+    
+color_groups = ColorGroupCycler()
 
 def extract_color(description):
     color = description.get('color')
     if color is None or color == 0:
         color = 'grey'
     else:
-        try:
-            num = int(color) - 1
-            replicate = int(description.get('replicate', 1)) - 1
-            color_group = color_groups()[num]
-            color = color_group[replicate % len(color_group)]
-        except ValueError:
-            color = 'grey'
+        num = int(color) - 1
+        replicate = int(description.get('replicate', 1)) - 1
+        color = color_groups[num, replicate]
 
     return color
         
