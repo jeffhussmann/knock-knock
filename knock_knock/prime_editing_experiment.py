3566276ea55380d507925c18d9537e81823792d4
Jeff Hussmann <jeff.hussmann@gmail.com>
2023-09-17 17:30:28 -0400

    removes dead code

--- knock_knock/prime_editing_experiment.py
+++ knock_knock/prime_editing_experiment.py
@@ -19,14 +19,6 @@ import knock_knock.Bxb1_layout
 from hits.utilities import memoized_property
 
 class PrimeEditingExperiment(knock_knock.illumina_experiment.IlluminaExperiment):
-    def __init__(self, base_dir, group, sample_name, **kwargs):
-        super().__init__(base_dir, group, sample_name, **kwargs)
-
-    @property
-    def read_types_to_align(self):
-        return ['nonredundant']
-        #return ['trimmed_by_name']
-
     @memoized_property
     def categorizer(self):
         return knock_knock.prime_editing_layout.Layout
@@ -37,110 +29,6 @@ class PrimeEditingExperiment(knock_knock.illumina_experiment.IlluminaExperiment)
         longest_seen = max((outcome.inferred_amplicon_length for outcome in outcomes), default=0)
         return max(min(longest_seen, 600), 100)
     
-    def make_nonredundant_sequence_fastq(self):
-        # This is overloaded by ArrayedExperiment.
-        fn = self.fns_by_read_type['fastq']['nonredundant']
-        with gzip.open(fn, 'wt', compresslevel=1) as fh:
-            for read in self.reads_by_type(self.preprocessed_read_type):
-                fh.write(str(read))
-
-    def generate_figures(self):
-        lengths_fig = self.length_distribution_figure()
-        lengths_fig.savefig(self.fns['lengths_figure'], bbox_inches='tight')
-        self.generate_all_outcome_length_range_figures()
-        self.generate_outcome_browser()
-        self.generate_all_outcome_example_figures(num_examples=5)
-
-    def alignment_groups_to_diagrams(self, alignment_groups, num_examples, **diagram_kwargs):
-        subsample = utilities.reservoir_sample(alignment_groups, num_examples)
-
-        for qname, als in subsample:
-            layout = self.categorizer(als, self.target_info, mode=self.layout_mode)
-
-            layout.categorize()
-            
-            try:
-                diagram = layout.plot(title='', **diagram_kwargs)
-            except:
-                print(self.sample_name, qname)
-                raise
-                
-            yield diagram
-
-    def extract_templated_insertion_info(self):
-        fields = knock_knock.prime_editing_layout.LongTemplatedInsertionOutcome.int_fields
-        
-        lists = defaultdict(list)
-
-        for outcome in self.outcome_iter():
-            if outcome.category == 'unintended donor integration':
-                insertion_outcome = knock_knock.prime_editing_layout.LongTemplatedInsertionOutcome.from_string(outcome.details)
-                
-                for field in fields: 
-                    value = getattr(insertion_outcome, field)
-                    key = f'{outcome.category}/{outcome.subcategory}/{field}'
-                    lists[key].append(value)
-                            
-        with h5py.File(self.fns['templated_insertion_details'], 'w') as hdf5_file:
-            cat_and_subcats = {key.rsplit('/', 1)[0] for key in lists}
-            read_length = 258
-            for cat_and_subcat in cat_and_subcats:
-                left_key = f'{cat_and_subcat}/left_insertion_query_bound'
-                right_key = f'{cat_and_subcat}/right_insertion_query_bound'
-
-                lengths = []
-
-                for left, right in zip(lists[left_key], lists[right_key]):
-                    if right == read_length - 1:
-                        length = read_length
-                    else:
-                        length = right - left + 1
-
-                    lengths.append(length)
-
-                lengths_key = f'{cat_and_subcat}/insertion_length'
-
-                lists[lengths_key] = lengths
-
-            for key, value_list in lists.items():
-                hdf5_file.create_dataset(f'{key}/list', data=np.array(value_list))
-
-                counts = Counter(value_list)
-
-                if len(counts) == 0:
-                    values = np.array([], dtype=int)
-                    counts = np.array([], dtype=int)
-                else:
-                    values = np.array(sorted(counts))
-                    counts = np.array([counts[v] for v in values])
-
-                hdf5_file.create_dataset(f'{key}/values', data=values)
-                hdf5_file.create_dataset(f'{key}/counts', data=counts)
-
-    def templated_insertion_details(self, category, subcategories, field):
-        counts = Counter()
-
-        if isinstance(subcategories, str):
-            subcategories = [subcategories]
-
-        with h5py.File(self.fns[f'templated_insertion_details']) as f:
-            for subcategory in subcategories:
-                group = f'{category}/{subcategory}/{field}'
-                if group in f:
-                    counts.update(dict(zip(f[group]['values'], f[group]['counts'])))
-
-        if knock_knock.layout.NAN_INT in counts:
-            counts.pop(knock_knock.layout.NAN_INT)
-
-        if len(counts) == 0:
-            xs = np.array([])
-        else:
-            xs = np.arange(min(counts), max(counts) + 1)
-
-        ys = np.array([counts[x] for x in xs])
-
-        return xs, ys
-
 class TwinPrimeExperiment(PrimeEditingExperiment):
     @memoized_property
     def categorizer(self):
