46bf6b7dfe4c440c67628b4cef540feff9d86072
Jeff Hussmann <jeff.hussmann@gmail.com>
2023-09-18 14:01:13 -0400

    removes unused code

--- knock_knock/integrases.py
+++ knock_knock/integrases.py
@@ -62,7 +62,7 @@ def identify_split_recognition_sequences(ref_seqs):
 
                     features[ref_name, full_name] = feature
 
-                    # Annotate the central dinucleotide next to the left half.
+                    # Annotate the central dinucleotide after the left half.
                     if side == 'left':
                         if strand == '+':
                             CD_start = match_end + 1
@@ -85,78 +85,6 @@ def identify_split_recognition_sequences(ref_seqs):
                                                            ID=full_name,
                                                            strand=strand,
                                                           )
-
-                    features[ref_name, full_name] = feature
-
-    return features
-
-def identify_recognition_sequences(ref_seqs):
-    features = {}
-
-    for ref_name, ref_seq in ref_seqs.items():
-        for source, site_seqs in recognition_sequences.items():
-            for site_name, site_seq in site_seqs.items():
-                pattern, *matches = Bio.SeqUtils.nt_search(ref_seq, site_seq)
-
-                site_seq_rc = hits.utilities.reverse_complement(site_seq)
-                pattern, *rc_matches = Bio.SeqUtils.nt_search(ref_seq, site_seq_rc)
-
-                if len(matches + rc_matches) > 1:
-                    raise NotImplementedError
-
-                central_dinucleotide_offset = site_seq.index('N')
-
-                feature_fields = []
-
-                for match in matches:
-                    feature_fields.extend([
-                        ('left',
-                         match,
-                         match + central_dinucleotide_offset - 1,
-                         '+',
-                        ),
-                        ('CD',
-                         match + central_dinucleotide_offset,
-                         match + central_dinucleotide_offset + 1,
-                        '+',
-                        ),
-                        ('right',
-                         match + central_dinucleotide_offset + 2, 
-                         match + len(site_seq) - 1,
-                         '+',
-                        ),
-                    ])
-
-                for rc_match in rc_matches:
-                    overall_end = rc_match + len(site_seq) - 1
-
-                    feature_fields.extend([
-                        ('left',
-                         overall_end - (central_dinucleotide_offset - 1),
-                         overall_end,
-                         '-',
-                        ),
-                        ('CD',
-                         overall_end - central_dinucleotide_offset - 1,
-                         overall_end - central_dinucleotide_offset,
-                         '-',
-                        ),
-                        ('right',
-                         rc_match,
-                         overall_end - central_dinucleotide_offset - 2,
-                         '-',
-                        ),
-                    ])
-
-                for name, start, end, strand in feature_fields:
-                    full_name = f'{source}_{site_name}_{name}'
-                    feature = hits.gff.Feature.from_fields(seqname=ref_name,
-                                                           start=start,
-                                                           end=end,
-                                                           ID=full_name,
-                                                           strand=strand,
-                                                          )
-                    feature.attribute['color'] = colors.get((site_name, name))
 
                     features[ref_name, full_name] = feature
 
