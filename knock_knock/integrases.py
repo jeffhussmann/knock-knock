0052e57b0db27dc80aff07c3cdf4158964687db9
Jeff Hussmann <jeff.hussmann@gmail.com>
2024-02-16 11:52:04 -0500

    improves annotation of integrase sites

--- knock_knock/integrases.py
+++ knock_knock/integrases.py
@@ -1,3 +1,6 @@
+import itertools
+from collections import defaultdict
+
 import bokeh.palettes
 import Bio.SeqUtils
 
@@ -26,96 +29,160 @@ colors = {
 }
 
 def identify_split_recognition_sequences(ref_seqs):
-    features = {}
+    all_features = {}
 
     for ref_name, ref_seq in ref_seqs.items():
         for source, site_seqs in split_recognition_sequences.items():
             for site_name, split_seq in site_seqs.items():
+                component_features = defaultdict(list)
+
                 for side, seq in split_seq.items(): 
+                    all_matches = []
+
                     pattern, *matches = Bio.SeqUtils.nt_search(ref_seq, seq)
+                    all_matches.extend([(match, '+') for match in matches])
 
                     seq_rc = hits.utilities.reverse_complement(seq)
                     pattern, *rc_matches = Bio.SeqUtils.nt_search(ref_seq, seq_rc)
+                    all_matches.extend([(match, '-') for match in rc_matches])
 
-                    if len(matches + rc_matches) > 1:
-                        # TODO: annotate multiple.
-                        return features
-
-                    if len(matches) == 1:
-                        strand = '+'
-                    elif len(rc_matches) == 1:
-                        strand = '-'
-                    else:
-                        continue
-
-                    match_start = (matches + rc_matches)[0]
-                    match_end = match_start + len(seq) - 1
-                    
-                    full_name = f'{source}_{site_name}_{side}'
-                    feature = hits.gff.Feature.from_fields(seqname=ref_name,
-                                                           start=match_start,
-                                                           end=match_end,
-                                                           ID=full_name,
-                                                           strand=strand,
-                                                          )
-                    feature.attribute['color'] = colors.get((site_name, side))
-
-                    features[ref_name, full_name] = feature
-
-                    # Annotate the central dinucleotide after the left half.
-                    if side == 'left':
-                        if strand == '+':
-                            CD_start = match_end + 1
-                        else:
-                            CD_start = match_start - 2
-                    elif side == 'right':
-                        if strand == '+':
-                            CD_start = match_start - 2
-                        else:
-                            CD_start = match_end + 1
-                    else:
-                        raise ValueError(side)
-
-                    CD_end = CD_start + 1
-
-                    if CD_start >= 0 and CD_end < len(ref_seq):
-                        full_name = f'{source}_{site_name}_CD'
+                    for match_start, strand in all_matches:
+                        match_end = match_start + len(seq) - 1
+                        
+                        full_name = f'{source}_{site_name}_{side}_{match_start}'
                         feature = hits.gff.Feature.from_fields(seqname=ref_name,
-                                                            start=CD_start,
-                                                            end=CD_end,
-                                                            ID=full_name,
-                                                            strand=strand,
-                                                            )
+                                                               start=match_start,
+                                                               end=match_end,
+                                                               ID=full_name,
+                                                               strand=strand,
+                                                              )
+                        feature.attribute['color'] = colors.get((site_name, side))
+                        feature.attribute['component'] = side
 
-                        features[ref_name, full_name] = feature
+                        component_features[side].append(feature)
+                        all_features[ref_name, full_name] = feature
 
-                left = features.get((ref_name, f'{source}_{site_name}_left'))
-                CD = features.get((ref_name, f'{source}_{site_name}_CD'))
-                right = features.get((ref_name, f'{source}_{site_name}_right'))
+                        # Annotate the central dinucleotide after the left half.
+                        if side == 'left':
+                            if strand == '+':
+                                CD_start = match_end + 1
+                            else:
+                                CD_start = match_start - 2
+                        elif side == 'right':
+                            if strand == '+':
+                                CD_start = match_start - 2
+                            else:
+                                CD_start = match_end + 1
+                        else:
+                            raise ValueError(side)
 
-                full_name = f'{source}_{site_name}'
+                        CD_end = CD_start + 1
 
-                if (left is not None and CD is not None and right is not None):
+                        if CD_start >= 0 and CD_end < len(ref_seq):
+                            full_name = f'{source}_{site_name}_CD_{CD_start}'
+                            feature = hits.gff.Feature.from_fields(seqname=ref_name,
+                                                                   start=CD_start,
+                                                                   end=CD_end,
+                                                                   ID=full_name,
+                                                                   strand=strand,
+                                                                  )
+                            feature.attribute['component'] = 'CD'
+
+                            component_features['CD'].append(feature)
+                            all_features[ref_name, full_name] = feature
+
+                for left, CD, right in itertools.product(component_features['left'], component_features['CD'], component_features['right']):
+                    full_name = f'{source}_{site_name}_{left.start}'
 
                     if left.strand == '+' and CD.strand == '+' and right.strand == '+':
-
                         if left.end + 1 == CD.start and CD.end + 1 == right.start:
-                            feature = hits.gff.Feature.from_fields(seqname=ref_name,
-                                                                   start=left.start,
-                                                                   end=right.end,
-                                                                   ID=full_name,
-                                                                   strand='+',
-                                                                  )
-                            features[ref_name, full_name] = feature
+                            strand = '+'
+                            start = left.start
+                            end = right.end
+                        else:
+                            continue
 
                     elif left.strand == '-' and CD.strand == '-' and right.strand == '-':
                         if left.start == CD.end + 1 and CD.start == right.end + 1:
-                            feature = hits.gff.Feature.from_fields(seqname=ref_name,
-                                                                   start=right.start,
-                                                                   end=left.end,
-                                                                   ID=full_name,
-                                                                   strand='-',
-                                                                  )
-                            features[ref_name, full_name] = feature
-                    
-    return features
+                            strand = '-'
+                            start = left.start
+                            end = right.end
+                        else:
+                            continue
+
+                    else:
+                       continue
+
+                    feature = hits.gff.Feature.from_fields(seqname=ref_name,
+                                                           start=start,
+                                                           end=end,
+                                                           ID=full_name,
+                                                           strand=strand,
+                                                          )
+
+                    for child in [left, CD, right]:
+                        feature.children.add(child)
+                        child.parent = feature
+
+                    feature.attribute['central_dinucleotide'] = CD.sequence(ref_seqs)
+                    feature.attribute['component'] = 'full_site'
+
+                    all_features[ref_name, full_name] = feature
+
+    return all_features
+
+def components(full_feature):
+    return {child.attribute['component']: child for child in full_feature.children}
+
+def recombine(ref_seqs, target, donor):
+    features = identify_split_recognition_sequences(ref_seqs)
+
+    sites = defaultdict(dict)
+
+    opposite_site = {
+        'attB': 'attP',
+        'attP': 'attB',
+    }
+
+    for (ref_name, feature_name), feature in features.items():
+        if feature.attribute['component'] == 'full_site':
+            source, site_name, *rest = feature_name.split('_')
+            sites[ref_name][source, site_name, feature.attribute['central_dinucleotide']] = feature
+
+    pairs = []
+
+    for source, which, CD in sites[target]:
+        if (source, opposite_site[which], CD) in sites[donor]:
+            pairs.append((sites[target][source, which, CD], sites[donor][source, opposite_site[which], CD]))
+            
+    if len(pairs) == 0:
+        raise ValueError
+
+    elif len(pairs) == 1:
+        target_site, donor_site = pairs[0]
+
+        donor_components = components(donor_site)
+        donor_seq = ref_seqs[donor]
+
+        if donor_site.strand == '+':
+            first_CD_p = donor_components['CD'].start
+            integrated_donor = donor_seq[first_CD_p + 1:] + donor_seq[:first_CD_p + 1]
+        else:
+            raise NotImplementedError
+            
+        target_components = components(target_site)
+        target_seq = ref_seqs[target]
+
+        if target_site.strand == '+':
+            first_CD_p = target_components['CD'].start
+            target_before = target_seq[:first_CD_p + 1]
+            target_after = target_seq[first_CD_p + 1:]
+        else:
+            raise NotImplementedError
+            
+        recombined_sequence = target_before + integrated_donor + target_after
+
+    else:
+        raise NotImplementedError
+
+    return recombined_sequence
